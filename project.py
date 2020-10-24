2123fe5a869a9831ee6ef65b6b6c7ce0428e440e
jar5030 <73356800+jar5030@users.noreply.github.com>
2020-10-23 19:26:51 -0500

    Add files via upload

--- project.py
+++ project.py
@@ -32,8 +32,8 @@ def get_actor(state_size):
     # if you use eager mode or autograph mode
     
     x = Input(shape=(state_size,))   
-    h0 = Dense(HIDDEN1_NODES, activation='relu')(x)
-    h1 = Dense(HIDDEN2_NODES, activation='relu')(h0)
+    h0 = Dense(hidden_unit1, activation='relu')(x)
+    h1 = Dense(hidden_unit2, activation='relu')(h0)
     Steering = Dense(
         1,
         activation='tanh',
@@ -57,15 +57,16 @@ def get_actor(state_size):
 def get_critic(state_size,action_space):
     # you don't either need to compile model here
     # just build the model
+    
     S = Input(shape=(state_size,))  
     A = Input(shape=(action_space,),name='action2')
     
-    w1 = Dense(HIDDEN1_NODES, activation='relu')(S)
-    a1 = Dense(HIDDEN2_NODES, activation='linear')(A) 
+    w1 = Dense(hidden_unit1, activation='relu')(S)
+    a1 = Dense(hidden_unit2, activation='linear')(A) 
     
-    h1 = Dense(HIDDEN2_NODES, activation='linear')(w1)
+    h1 = Dense(hidden_unit2, activation='linear')(w1)
     h2 = add([h1,a1])    
-    h3 = Dense(HIDDEN2_NODES, activation='relu')(h2)
+    h3 = Dense(hidden_unit2, activation='relu')(h2)
     
     V = Dense(3,activation='linear')(h3)   
     model = ks.Model(inputs=[S,A],outputs=V)
@@ -88,21 +89,47 @@ def train_target(target_weights, weights, tau):
 # function for everything else
 def trainTorcs(train_indicator=1): # if 1 , it will train the model,if 0, it will use train model and run the ai driver
     # --------------------- declare all variables here-------------------------------------
+    #All these variables come from DDPG-Keras-Torcs ddpg.py
+    BUFFER_SIZE=100000
+    BATCH_SIZE=32
+    GAMMA=0.99
+    TAU=0.001
+    LRA=0.0001
+    LRC=0.001
     
+    action_dim=3
+    state_dim=29
+    
+    vision=False
+    
+    EXPLORE=100000.
+    max_episode=2000
+    max_steps=100000
+    reward=0
+    done=False
+    step=0
+    epsilon=1
+    indicator=0
+    
+    #I don't know what these should be??
+    hidden_unit1=1
+    hidden_unit2=2
     #-----------------------Create buffer here-----------------------
     
     # TODO: add noise 
     
     buff = ReplayBuffer(BUFFER_SIZE)    #Create replay buffer, using Replay buffer class
     # create model for actor and critic
-    actor_model  = get_actor(hidden_unit1, hidden_unit2)
+    #actor_model  = get_actor(hidden_unit1, hidden_unit2)
+    actor_model = get_actor(hidden_unit1)
     critic_model = get_critic(hidden_unit1, hidden_unit2)
     
     #actor_model.summary()
     #critic_model.summary()
     
     # create target actor and critic
-    target_actor = get_actor(hidden_unit1, hidden_unit2)
+    #target_actor = get_actor(hidden_unit1, hidden_unit2)
+    target_actor = get_actor(hidden_unit1)
     target_critic = get_critic(hidden_unit1, hidden_unit2)
     
     # initialize target weights same as acot and critic (default) weights
@@ -128,35 +155,47 @@ def trainTorcs(train_indicator=1): # if 1 , it will train the model,if 0, it wil
     except:
         print("Cannot find the weight")
       
-        #--------------------- from here we will use loops to tain model---------------------------
+    #--------------------- from here we will use loops to tain model---------------------------
       
-        for i in range(max_episode):
-
-            # TODO: follow code in torcs
+    for i in range(max_episode):
+        print("Episode #%d:" % (i+1))
+        # TODO: follow code in torcs
+        # Kill and restart TORCS every 3 episodes
+        if(np.mod(i, 3) == 0):
+            ob = env.reset(relaunch=True)
+        else:
+            ob = env.reset
         
-            for j in range(max_steps):
-                pass
-                # TODO: get state
-                
-                # TODO: generate action
-                # tf.convert_to_tensor(states, dtype=tf.float32)
-                #actions = actor_model(states)
-                  
-                # TODO: add noise to actions
-                  
-                
-                
-                # ob, r_t, done, info = env.step(a_t[0])
-                  
-                # TODO: save it to buffer
-                  
-                  
-                #------------------------ get states actions from buffer---------------------------
-                  
-                #TODO tain model --- call trainmodel
-                  
-                  
-                # rest will be same as in DDPG
+        reward = 0
+        
+        for j in range(max_steps):
+            # TODO: get state
+            state = env.get_obs()
+            #print("Current speed (X,Y,Z):")
+            #print(state.speedX, state.speedY, state.speedZ)
+            
+            
+            # TODO: generate action
+            #tf.convert_to_tensor(states, dtype=tf.float32)
+            #actions = actor_model(states)
+            #print("actions:")
+            #print(actions)
+              
+            # TODO: add noise to actions
+            
+            
+            
+            #ob, reward, done, info = env.step(a_t[0])
+            
+            # TODO: save it to buffer
+              
+              
+            #------------------------ get states actions from buffer---------------------------
+              
+            #TODO tain model --- call trainmodel
+              
+              
+            # rest will be same as in DDPG
 
 
 
